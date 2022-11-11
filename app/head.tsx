db6e1aa508f78a9bae578bef5b5e68cfc27bf181
rbelling <riccardo.bellingeri@gmail.com>
2022-11-11 14:59:36 -0500

    feat: add scaffolding for app folder

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Next.js + Markdown</title>
+      <meta
+        name="description"
+        content="A Next.js based boilerplate that allows you to use markdown files."
+      />
+    </>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+export default function Home() {
+  return (
+    <h1 className="text-green-500" data-testid="heading">
+      Hi therezjfk!
+    </h1>
+  );
+}
